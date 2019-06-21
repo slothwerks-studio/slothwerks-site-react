c730af80c48857115c392a48fd4ab00ae01f0a37
Sloth I <github@slothwerks.com>
2019-06-21 06:39:20 -0400

    Added sandwich menu (sans styles)

--- slothwerks-site/src/components/nav/sandwich-menu/sandwich-menu.js
+++ slothwerks-site/src/components/nav/sandwich-menu/sandwich-menu.js
@@ -13,10 +13,72 @@ import { Link } from "react-router-dom";
 export default function SandwichMenu(props) {
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </nav>
+    <div className="sandwich-menu">
+
+      <div className="sandwich-menu-header">
+        <Link 
+          to="/" 
+          className="header-logo-container"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          SlothWerks
+        </Link>
+        <span 
+          className="close-icon-container"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          <img 
+            className="contact-icon"
+            src={require('../../../assets/icons/close-white.png')} 
+          />
+        </span>
+      </div>
+
+      <div className="sandwich-menu-links">
+        <Link 
+          to="/about" 
+          class="menu-link"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          About
+        </Link>
+        <Link 
+          to="/audio-archives" 
+          class="menu-link"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          Audio Archives
+        </Link>
+        <Link 
+          to="/ena-calendar" 
+          class="menu-link"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          ENA Community Calendar
+        </Link>
+        <Link 
+          to="/tech-calendar" 
+          class="menu-link"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          Grand Circus Tech Calendar
+        </Link>
+        <Link 
+          to="/contact" 
+          class="menu-link"
+          onClick={()=>props.toggleSandwichMenu()}
+        >
+          Contact
+        </Link>
+      </div>
+
+      <div className="sandwich-menu-footer">
+        <span className="copyright">
+          &copy;SlothWerks LLC
+        </span>
+      </div>
+
+    </div>
   );
 
 }
