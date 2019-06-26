66927fa1931bbf75756d6ef1e7abe04998d93967
Sloth I <github@slothwerks.com>
2019-06-26 06:22:44 -0400

    Completed anbout view (mobile)

--- slothwerks-site/src/views/about/about.js
+++ slothwerks-site/src/views/about/about.js
@@ -12,7 +12,23 @@ export default function About() {
 
   return (
     <div className="About">
-      About
+      <h2 className="page-heading">
+        <span className="page-heading-text">About SlothWerks</span>
+        <span className="page-heading-divider"></span>
+      </h2>
+      <div className="content-text-wrapper">
+        <p className="content-text">
+          Located in <span className="bold">Grand Rapids, Michigan,</span> SlothWerks provides web and mobile application development and user experience consultation services.
+        </p>
+        <p className="content-text">
+          We harness twenty years of technical experience to craft solutions for both commercial and non-profit clients throughout West Michigan.
+        </p>
+      </div>
+      <div className="button-wrapper">
+        <button type="button" className="action-button">
+          Contact us!
+        </button>
+      </div>
     </div>
   );
 
