179ad93dac83b54e2550400f2fb5d1ae7acd36c9
Sloth I <github@slothwerks.com>
2019-06-28 07:02:39 -0400

    Completed ENA Calendar view (mobile)

---
+++ slothwerks-site/src/views/ena-calendar/ena-calendar.js
@@ -0,0 +1,66 @@
+import React, { useState, Fragment } from 'react';
+import { Link } from "react-router-dom";
+
+/* 
+  Component Description:
+    This is the ENA calendar screen view.
+
+  Incoming Props:
+    None
+*/
+
+export default function ENA_Calendar() {
+
+  return (
+    <div className="ENA_Calendar">
+      <h2 className="page-heading">
+        <span className="page-heading-text">ENA Community Calendar</span>
+        <span className="page-heading-divider"></span>
+      </h2>
+      <div className="content-text-wrapper">
+        <p className="content-text">
+          SlothWerks maintains a calendar of family-free (and usually free) community events occurring in the <a href="https://www.eastgategr.com/" target="_blank">Eastgate neighborhood</a> and throughout the city of Grand Rapids, Michigan.
+        </p>
+      </div>
+      <div className="sub-section-wrapper">
+        <h3 className="sub-section-heading">
+          About the Eastgate Neighborhood Association
+        </h3>
+        <p className="sub-section-text">
+          The ENA serves the Plymouth Heights area in Grand Rapids and offers special event programming, community outreach, and neighborhood resources to ENA residents.
+        </p>
+        <div className="calendar-button-section">
+          <div className="calendar-button-wrapper">
+            <div className="calendar-button-text-container">
+              <img
+                className="calendar-button-image"
+                src={require('../../assets/icons/external-link-black.png')} 
+              />
+              <span className="calendar-button-text">
+                View Event Calendar
+              </span>
+            </div>
+            <button type="button" className="action-button">
+              <a>View Calendar</a>
+            </button>
+          </div>
+          <div className="calendar-button-wrapper">
+            <div className="calendar-button-text-container">
+              <img
+                className="calendar-button-image"
+                src={require('../../assets/icons/calendar-add-black.png')} 
+              />
+              <span className="calendar-button-text">
+                Add to Google Calendar
+              </span>
+            </div>
+            <button type="button" className="action-button">
+              <a>Add</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+}
