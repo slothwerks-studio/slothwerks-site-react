573d5609523897e7228a762573862228d448c762
Sloth I <github@slothwerks.com>
2019-07-01 09:01:48 -0400

    Added initial JSX for audio archives card

--- slothwerks-site/src/views/audio-archives/audio-archives.js
+++ slothwerks-site/src/views/audio-archives/audio-archives.js
@@ -23,7 +23,36 @@ export default function Audio_Archives() {
         </p>
       </div>
       <div className="audio-archives-wrapper">
-        
+        <div className="audio-archives-card-container">
+          <div className="audio-archives-card-image-container">
+            <img 
+              className="audio-archives-card-image"
+              src={require('../../assets/images/hack-show-2016.jpg')} 
+              alt="Turntable and mixing board"
+            />
+          </div>
+          <div className="audio-archives-card-info-container">
+            <div className="audio-archives-card-info">
+              <span className="audio-archives-card-title">
+                The Sloth Psychedelic HACK! Show (2016 Vinyl Edition)
+              </span>
+              <span className="audio-archives-info-text">
+                <span className="bold">Released:</span> 2016
+              </span>
+              <span className="audio-archives-info-text">
+                <span className="bold">Total Time:</span> 46 minutes
+              </span>
+            </div>
+            <div class="audio-player">
+              <audio controls>
+                <source src="https://www.slothwerks.com/media/new/sloth-psychedelic-hack-show-2016.mp3" type="audio/mp3" />
+                <p class="no-audio-support">
+                  Your browser doesn't support HTML5 audio. Access the source file directly <a href="https://www.slothwerks.com/media/new/sloth-psychedelic-hack-show-2016.mp3">here</a>.
+                </p>
+              </audio>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
