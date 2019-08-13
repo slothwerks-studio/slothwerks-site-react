3d1c17aff0e57f3e8cbf55f885775488f00a4804
Sloth I <github@slothwerks.com>
2019-08-13 07:49:35 -0400

    Added desktop styles for audio archives - will need testing at various screen widths

--- slothwerks-site/src/views/audio-archives/components/audio-archives-card.js
+++ slothwerks-site/src/views/audio-archives/components/audio-archives-card.js
@@ -26,19 +26,29 @@ export default function AudioArchivesCard(props) {
             alt={props.imageAlt}
           />
         </div>
-        <div className="audio-archives-card-info">
-          <span className="audio-archives-card-title">
-            {props.title}
-          </span>
-          <span className="audio-archives-info-text">
-            <span className="bold">Released:</span> {props.releaseDate}
-          </span>
-          <span className="audio-archives-info-text">
-            <span className="bold">Total Time:</span> {props.totalTime}
-          </span>
+        <div className="audio-archives-card-info-wrapper">
+          <div className="audio-archives-card-info">
+            <span className="audio-archives-card-title">
+              {props.title}
+            </span>
+            <span className="audio-archives-info-text">
+              <span className="bold">Released:</span> {props.releaseDate}
+            </span>
+            <span className="audio-archives-info-text">
+              <span className="bold">Total Time:</span> {props.totalTime}
+            </span>
+          </div>
+          <div className="audio-player-inside">
+            <audio controls className="audio-controls">
+              <source src={props.audioLocation} type="audio/mp3" />
+              <p className="no-audio-support">
+                Your browser doesn't support HTML5 audio. Access the source file directly <a href="https://www.slothwerks.com/media/new/sloth-psychedelic-hack-show-2016.mp3">here</a>.
+              </p>
+            </audio>
+          </div>
         </div>
       </div>
-      <div className="audio-player">
+      <div className="audio-player-outside">
         <audio controls className="audio-controls">
           <source src={props.audioLocation} type="audio/mp3" />
           <p className="no-audio-support">
